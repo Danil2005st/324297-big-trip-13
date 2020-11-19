ded99df9123d63eb05ed023871e25b930ad6b10a
danila.prokopenko <DanPila12>
2020-11-19 21:12:05 +0200

    Разбивка на модули исправление ошибок

--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -1,7 +1,7 @@
-export const createTripInfo = () => {
-  return `<div class="trip-info__main">
-              <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
+export const createTripInfo = () =>
+  `<div class="trip-info__main">
+    <h1 class="trip-info__title">Amsterdam &mdash; Chamonix &mdash; Geneva</h1>
 
-              <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
-            </div>`;
-};
+    <p class="trip-info__dates">Mar 18&nbsp;&mdash;&nbsp;20</p>
+  </div>`
+;
